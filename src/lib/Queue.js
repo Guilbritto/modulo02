bf64928caa8d379ce40af9e9079864a8e4a38c95
Guilherme Brito <guilhermebritto.prof@gmail.com>
2019-08-26 23:29:20 -0300

    Conclusão da aplicação de backend

---
+++ src/lib/Queue.js
@@ -0,0 +1,41 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationsMail';
+import radisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: radisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  handleFailure(job, err) {
+    console.log(`Queue ${job.queue.name}: FAILED`, err);
+  }
+
+  proccessQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+      bee.on('failed', this.handleFailure).process(handle);
+    });
+  }
+}
+
+export default new Queue();
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,39 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
+
+import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Guilherme brito',
-    email: 'guilhermebritto.prof@gmail.com',
-    password_hash: '12313123123',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.strore);
 
-  return res.json(user);
-});
+routes.use(authMiddleware);
 
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
